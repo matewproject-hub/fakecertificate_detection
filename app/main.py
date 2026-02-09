b1f4d0e6232a23a97502478bd153edc98206ea0d
matewproject-hub <matewproject@gmail.com>
2026-02-09 19:37:19 +0530

    backend only

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from app.models import Certificate
 from app.utils import generate_hash
 from app.qr_utils import extract_qr
 # app/main.py
-from fastapi.staticfiles import StaticFiles
+
 
 
 
@@ -18,7 +18,7 @@ Base.metadata.create_all(bind=engine)
 
 app=FastAPI(title="Fake Certificate Detection")
 
-app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
+
 
 
 app.add_middleware(
@@ -85,24 +85,37 @@ async def verify_certificate(
     file: UploadFile = File(...),
     db: Session = Depends(get_db)
 ):
-
-
     data = await file.read()
     new_hash = generate_hash(data)
+    
+    # Try to extract cert UID (optional)
+    qr_data = extract_qr(data)
+    cert_uid = None
+    if qr_data:
+        parts = qr_data.split("|")
+        cert_uid = parts[1]
 
+    # Case 1: Exact hash exists → VALID
     found = db.query(Certificate).filter(Certificate.hash == new_hash).first()
-
     if found:
         return {
             "status": "VALID",
             "certificate_id": found.id,
             "filename": found.filename
         }
+    
+    # Case 2: Tampered → same cert_uid exists but hash changed
+    if cert_uid:
+        tampered = db.query(Certificate).filter(Certificate.cert_uid == cert_uid).first()
+        if tampered:
+            return {
+                "status": "TAMPERED",
+                "certificate_id": tampered.id,
+                "filename": tampered.filename
+            }
 
-    return {
-        "status": "TAMPERED / NOT FOUND"
-    }
-
+    # Case 3: Not found → no matching cert_uid or hash
+    return {"status": "NOT FOUND"}
 
     
     
